ddd8b760bb1f1f5dcedba630fc7a37f66b88860e
Olof Petterson <olof.petterson@vinngroup.se>
2020-12-10 07:37:44 +0100

    Days 9 and 10

---
+++ net/AoC2020/Solutions/Day09.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+
+namespace AoC2020.Solutions
+{
+    public class Day09 : BaseDay
+    {
+        public override string SolveA()
+        {
+            var longs = lines.Select(long.Parse).ToArray();
+            var result = Run(longs);
+            return result != -1 ? result.ToString() : "no solution";
+        }
+
+        public override string SolveB()
+        {
+            var longs = lines.Select(long.Parse).ToArray();
+            var invalidNumber = Run(longs);
+
+            for (var i = 0; i < longs.Length; i++)
+            {
+                long sum = 0;
+                for (var j = i; j < longs.Length; j++)
+                {
+                    sum += longs[j];
+                    if (sum == invalidNumber)
+                    {
+                        return (longs[i..j].Min() + longs[i..j].Max()).ToString();
+                    }
+
+                    if (sum > invalidNumber)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return "no solution";
+        }
+
+        private static long Run(long[] longs)
+        {
+            for (var i = 25; i < longs.Length; i++)
+            {
+                var found = false;
+                for (var j = i - 1; j >= i - 24; j--)
+                {
+                    for (var k = j - 1; k >= i - 25; k--)
+                    {
+                        if (longs[j] + longs[k] == longs[i])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found)
+                    {
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    return longs[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ net/AoC2020/Solutions/Day10.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Solutions
+{
+    public class Day10 : BaseDay
+    {
+        public override string SolveA()
+        {
+            var adapters = lines.Select(int.Parse).ToList();
+            adapters.AddRange(new[] {0, adapters.Max() + 3});
+            adapters.Sort();
+
+            var ones = 0;
+            var threes = 0;
+
+            for (var i = 1; i < adapters.Count; i++)
+            {
+                if (adapters[i] - adapters[i - 1] == 1)
+                    ones++;
+                if (adapters[i] - adapters[i - 1] == 3)
+                    threes++;
+            }
+
+            return (ones * threes).ToString();
+        }
+
+        public override string SolveB()
+        {
+            var adapters = lines.Select(int.Parse).ToList();
+            adapters.AddRange(new[] {0, adapters.Max() + 3});
+            adapters.Sort();
+            adapters.Reverse();
+            
+            var connections = new Dictionary<int, long>();
+            
+            foreach(var adapter in adapters)
+            {
+                connections[adapter] = Math.Max(adapters.Where(x => x > adapter && x <= adapter + 3).Sum(x => connections[x]), 1);
+            }
+
+            return connections[0].ToString();
+        }
+    }
+}
