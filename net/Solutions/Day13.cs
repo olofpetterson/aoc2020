c5057fc639135af36716e708d5039553ee21237b
Olof Petterson <olof.petterson@vinngroup.se>
2020-12-13 08:58:19 +0100

    Day 13

---
+++ net/Solutions/Day13.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Solutions
+{
+    public class Day13 : BaseDay
+    {
+        public override string SolveA()
+        {
+            var start = int.Parse(lines[0]);
+            var buses = lines[1].Split(",").Where(x => x != "x").Select(int.Parse);
+            var min = int.MaxValue;
+            var minBus = 0;
+
+            foreach (var bus in buses)
+            {
+                var before = start / bus;
+                var diff = (before + 1) * bus - start;
+                if (diff < min)
+                {
+                    min = diff;
+                    minBus = bus;
+                }
+            }
+
+            return (minBus * min).ToString();
+        }
+
+        public override string SolveB()
+        {
+            var buses = new List<(long, long)>();
+            lines[1].Split(",").Aggregate(0, (index, bus) =>
+            {
+                if (bus != "x")
+                {
+                    buses.Add((long.Parse(bus), index));
+                }
+
+                return index + 1;
+            });
+
+            long departure = 0;
+            buses.OrderByDescending(x => x.Item1).Aggregate(buses[0].Item1, (cycle, bus) =>
+            {
+                while ((departure + bus.Item2) % bus.Item1 != 0)
+                {
+                    departure += cycle;
+                }
+
+                return cycle * bus.Item1;
+            });
+
+            return departure.ToString();
+        }
+    }
+}
