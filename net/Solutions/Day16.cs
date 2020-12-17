0773f9ddef112927b53681a5dc4e9bf268af34aa
Olof Petterson <olof.petterson@vinngroup.se>
2020-12-17 08:01:08 +0100

    Day 16, 17

---
+++ net/Solutions/Day16.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Solutions
+{
+    public class Day16 : BaseDay
+    {
+        public override string SolveA()
+        {
+            var rules = new List<(string, (int, int), (int, int))>();
+            var i = 0;
+            for (; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    break;
+                }
+                var split = lines[i].Split(new[] {':', '-'}, StringSplitOptions.RemoveEmptyEntries);
+                var split2 = split[2].Split(" or ");
+                rules.Add((split[0], (int.Parse(split[1]), int.Parse(split2[0])), (int.Parse(split2[1]), int.Parse(split[3]))));
+            }
+
+            var rate = 0;
+            for (i += 5; i < lines.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    var split = lines[i].Split(',').Select(int.Parse);
+                    
+                    foreach (var value in split)
+                    {
+                        var isValid = false;
+                        foreach (var (_, (firstLow, firstHigh), (secondLow, secondHigh)) in rules)
+                        {
+                            if (value >= firstLow && value <= firstHigh ||
+                                value >= secondLow && value <= secondHigh)
+                            {
+                                isValid = true;
+                                break;
+                            }
+                        }
+
+                        if (!isValid)
+                        {
+                            rate += value;
+                        }
+                    }
+                }
+            }
+
+            return rate.ToString();
+        }
+
+        public override string SolveB()
+        {
+            var rules = new List<(string, (int, int), (int, int))>();
+            var i = 0;
+            for (; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    break;
+                }
+                var split = lines[i].Split(new[] {':', '-'}, StringSplitOptions.RemoveEmptyEntries);
+                var split2 = split[2].Split(" or ");
+                rules.Add((split[0], (int.Parse(split[1]), int.Parse(split2[0])), (int.Parse(split2[1]), int.Parse(split[3]))));
+            }
+
+            var myTicket = lines[i + 2].Split(",").Select(int.Parse).ToArray();
+            var dict = rules.ToDictionary(x => x.Item1, x => new List<int>());
+            var validTickets = new HashSet<int>();
+ 
+            for (i += 5; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    continue;
+                }
+                
+                var split = lines[i].Split(',').Select(int.Parse).ToArray();
+                var isTicketValid = true;
+                foreach (var value in split)
+                {
+                    var isValueValid = false;
+                    foreach (var (_, (firstLow, firstHigh), (secondLow, secondHigh)) in rules)
+                    {
+                        if (value >= firstLow && value <= firstHigh ||
+                            value >= secondLow && value <= secondHigh)
+                        {
+                            isValueValid = true;
+                        }
+                    }
+
+                    if (!isValueValid)
+                    {
+                        isTicketValid = false;
+                        break;
+                    }
+                }
+
+                if (isTicketValid)
+                {
+                    validTickets.Add(i);
+                }
+            }
+
+            foreach (var (ruleName, (firstLow, firstHigh), (secondLow, secondHigh)) in rules)
+            {
+                for (var j = 0; j < rules.Count; j++)
+                {
+                    var isValid = true;
+                    foreach (var validTicket in validTickets)
+                    {
+                        var split = lines[validTicket].Split(',').Select(int.Parse).ToArray();
+                        var value = split[j];
+                        if (!(value >= firstLow && value <= firstHigh ||
+                              value >= secondLow && value <= secondHigh))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        dict[ruleName].Add(j);
+                    }
+                }
+            }
+
+            var ruleIndex = new Dictionary<string, int>();
+            foreach (var (key, value) in dict.OrderBy(x => x.Value.Count))
+            {
+                if (value.Except(ruleIndex.Values).Count() == 1)
+                {
+                    ruleIndex[key] = value.Except(ruleIndex.Values).Single();
+                }
+            }
+            return ruleIndex.Where(x => x.Key.StartsWith("departure")).Aggregate(1L, (product, x) => myTicket[x.Value] * product).ToString();
+        }
+    }
+}
---
+++ net/Solutions/Day17.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020.Solutions
+{
+    public class Day17: BaseDay
+    {
+        public override string SolveA()
+        {
+            var dict = new Dictionary<(int x, int y, int z), bool>();
+            for (var y = 0; y < lines.Length; y++)
+            {
+                for (var x = 0; x < lines[0].Length; x++)
+                {
+                    dict[(x, y, 0)] = lines[y][x] == '#';
+                }
+            }
+
+            var range = Enumerable.Range(-1, 3).ToArray();
+            var moves = (from z in range from y in range from x in range where x != 0 || y != 0 || z != 0 select (x, y, z)).ToList();
+
+            for (var i = 0; i < 6; i++)
+            {
+                var newDict = new Dictionary<(int, int, int), bool>();
+                for (var z = dict.Keys.Min(x => x.z) -1; z < dict.Keys.Max(x => x.z) + 2; z++)
+                {
+                    for (var y = dict.Keys.Min(x => x.y) - 1; y < dict.Keys.Max(x => x.y) + 2; y++)
+                    {
+                        for (var x = dict.Keys.Min(a => a.x) - 1; x < dict.Keys.Max(a => a.x) + 2; x++)
+                        {
+                            var count = 0;
+                            foreach (var move in moves)
+                            {
+                                if (dict.TryGetValue((x + move.x, y + move.y, z + move.z), out var active))
+                                {
+                                    count += active ? 1 : 0;
+                                }
+                            }
+
+                            dict.TryGetValue((x, y, z), out var isActive);
+                            if (isActive)
+                            {
+                                if (count == 2 || count == 3)
+                                {
+                                    newDict[(x, y, z)] = true;
+                                }
+                                else
+                                {
+                                    newDict[(x, y, z)] = false;
+                                }
+                            }
+                            else
+                            {
+                                if (count == 3)
+                                {
+                                    newDict[(x, y, z)] = true;
+                                }
+                                else
+                                {
+                                    newDict[(x, y, z)] = false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                dict = newDict;
+            }
+
+            return dict.Count(x => x.Value).ToString();
+        }
+
+        public override string SolveB()
+        {
+            var dict = new Dictionary<(int x, int y, int z, int w), bool>();
+            for (var y = 0; y < lines.Length; y++)
+            {
+                for (var x = 0; x < lines[0].Length; x++)
+                {
+                    dict[(x, y, 0, 0)] = lines[y][x] == '#';
+                }
+            }
+
+            var range = Enumerable.Range(-1, 3).ToArray();
+            var moves = (from w in range from z in range from y in range from x in range where x != 0 || y != 0 || z != 0 || w != 0 select (x, y, z, w)).ToList();
+
+            for (var i = 0; i < 6; i++)
+            {
+                var newDict = new Dictionary<(int, int, int, int), bool>();
+                for (var w = dict.Keys.Min(x => x.w) - 1; w < dict.Keys.Max(x => x.w) + 2; w++)
+                {
+                    for (var z = dict.Keys.Min(x => x.z) - 1; z < dict.Keys.Max(x => x.z) + 2; z++)
+                    {
+                        for (var y = dict.Keys.Min(x => x.y) - 1; y < dict.Keys.Max(x => x.y) + 2; y++)
+                        {
+                            for (var x = dict.Keys.Min(a => a.x) - 1; x < dict.Keys.Max(a => a.x) + 2; x++)
+                            {
+                                var count = 0;
+                                foreach (var move in moves)
+                                {
+                                    if (dict.TryGetValue((x + move.x, y + move.y, z + move.z, w + move.w),
+                                        out var active))
+                                    {
+                                        count += active ? 1 : 0;
+                                    }
+                                }
+
+                                dict.TryGetValue((x, y, z, w), out var isActive);
+                                if (isActive)
+                                {
+                                    if (count == 2 || count == 3)
+                                    {
+                                        newDict[(x, y, z, w)] = true;
+                                    }
+                                    else
+                                    {
+                                        newDict[(x, y, z, w)] = false;
+                                    }
+                                }
+                                else
+                                {
+                                    if (count == 3)
+                                    {
+                                        newDict[(x, y, z, w)] = true;
+                                    }
+                                    else
+                                    {
+                                        newDict[(x, y, z, w)] = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                dict = newDict;
+            }
+        
+
+            return dict.Count(x => x.Value).ToString();
+        }
+    }
+}
